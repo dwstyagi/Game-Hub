d164c483505f877eba840d7954f91549075a47b4
Diwanshu Tyagi <Diwanshu@healthy18.com>
2024-03-22 16:06:52 +0530

    display platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,46 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMapper: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ninento: SiNintendo,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY="10px">
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMapper[platform.slug]}
+          color="gray.500"
+        />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
