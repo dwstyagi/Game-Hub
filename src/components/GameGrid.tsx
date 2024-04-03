ff9604813b300ff8f64388ec534543cefa627905
Diwanshu Tyagi <Diwanshu@healthy18.com>
2024-04-03 17:42:45 +0530

    fixing issues with chakras menus

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,11 +16,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={10}
-        padding={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 4 }} spacing={10} padding={10}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
